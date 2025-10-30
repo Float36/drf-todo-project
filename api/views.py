0c51c4da2c1e20c1c5d9127b9ef825a8a7bfb51c
Yura <yurastashchyshyn@gmail.com>
2025-10-30 12:39:23 +0200

    Feat: Add TodoList and TodoDetail generic views with URLs

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    """
+    GET for all tasks, POST for new task
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET PUT PATCH DELETE by id
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+
